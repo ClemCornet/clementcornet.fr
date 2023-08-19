8ec91ab809963e910e7fd9315408fd9fbe9eb1d7
ClemCornet <clement.cornet@lecollectionist.com>
2023-08-19 22:48:49 +0200

    [mobile] remove toogleTheme from topNav and add sticky btn

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -17,7 +17,7 @@ export const TopNavigation = () => {
 
   return (
     <div
-      className={`container sticky top-10 mx-auto flex max-w-2xl items-center justify-between transition duration-300 ${opacity} z-50 rounded-2xl border border-slate-200 px-4 py-4 backdrop-blur will-change-transform dark:border-none [@supports(backdrop-filter:blur(0px))]:bg-white/[3%]`}
+      className={`container sticky top-10 mx-auto flex max-w-2xl items-center justify-around transition duration-300 md:justify-between ${opacity} z-50 rounded-2xl border border-slate-200 px-4 py-4 backdrop-blur will-change-transform dark:border-none [@supports(backdrop-filter:blur(0px))]:bg-white/[3%]`}
     >
       <div className="h-[50px] w-[50px] rounded-full ring-[4px] ring-blue/90 dark:ring-blue/60">
         <img
@@ -28,7 +28,7 @@ export const TopNavigation = () => {
       </div>
       <div className={`flex items-center justify-center text-slate-400`}>
         <HeroNavigation />
-        <div className="ml-6">
+        <div className="ml-6 hidden md:block">
           <ToggleTheme iconsSize="1.2em" />
         </div>
       </div>
