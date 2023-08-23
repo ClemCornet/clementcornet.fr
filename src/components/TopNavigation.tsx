09e42be4d1f5a3997e9213c36e22e28732517c48
ClemCornet <clement.cornet@lecollectionist.com>
2023-08-23 23:45:20 +0200

    implement rooting app + transition views

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -6,28 +6,38 @@ import { navigationStore } from "../stores/navigationStore"
 import { HeroNavigation } from "./HeroNavigation"
 import { ToggleTheme } from "./ToggleTheme"
 
-export const TopNavigation = () => {
+export const TopNavigation = ({
+  currentPath,
+  isStatic = false,
+}: {
+  currentPath: string
+  isStatic?: boolean
+}) => {
   const navigation = useStore(navigationStore)
 
-  const opacity = useMemo(
-    () =>
-      navigation === "static" ? "hidden scale-0" : "opacity-100 scale-100",
-    [navigation]
-  )
+  const opacity = useMemo(() => {
+    if (isStatic) {
+      return "opacity-100"
+    }
+    return navigation === "static" ? "hidden scale-0" : "opacity-100 scale-100"
+  }, [navigation])
 
   return (
     <div
-      className={`container sticky top-8 mx-auto flex max-w-2xl items-center justify-around transition duration-300 md:justify-between ${opacity} z-50 rounded-2xl border border-slate-200 px-4 py-4 backdrop-blur will-change-transform dark:border-none [@supports(backdrop-filter:blur(0px))]:bg-white/[3%]`}
+      className={`container sticky top-8 mx-auto flex max-w-2xl items-center justify-around transition duration-300 md:justify-between ${opacity} z-50 rounded-2xl border border-slate-300 px-4 py-4 backdrop-blur will-change-transform dark:border-none [@supports(backdrop-filter:blur(0px))]:bg-white/[3%]`}
     >
-      <div className="h-[50px] w-[50px] rounded-full ring-[4px] ring-blue/90 dark:ring-blue/60">
+      <a
+        href="/"
+        className="h-[50px] w-[50px] rounded-full ring-[4px] ring-blue/90 dark:ring-blue/60"
+      >
         <img
           src="/assets/profile-picture.png"
           alt="Profile picture"
           className="h-full w-full rounded-full"
         />
-      </div>
+      </a>
       <div className={`flex items-center justify-center text-slate-400`}>
-        <HeroNavigation />
+        <HeroNavigation currentPath={currentPath} />
         <div className="ml-6 hidden md:block">
           <ToggleTheme iconsSize="1.2em" />
         </div>
